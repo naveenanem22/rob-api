5b0fbbf16664788a98ad4f7bd93e49a5bf956f20
Naveen Anem <KH2475@kitspl.com>
2018-10-17 12:15:34 +0530

    Custom deserializer for Employee in JobPost

---
+++ src/main/java/com/rob/common/EmployeeInJobPostDeSerializer.java
@@ -0,0 +1,26 @@
+package com.rob.common;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.pmt.model.Employee;
+
+public class EmployeeInJobPostDeSerializer extends JsonDeserializer<Employee> {
+
+	@Override
+	public Employee deserialize(JsonParser jp, DeserializationContext ctxt)
+			throws IOException, JsonProcessingException {
+
+		TreeNode treeNode = jp.getCodec().readTree(jp);
+		TextNode employeeId = (TextNode) treeNode.get("employeeId");
+		Employee employee = new Employee();
+		employee.setId(employeeId.asText());
+		return employee;
+	}
+
+}
